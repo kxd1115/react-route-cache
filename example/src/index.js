34ecfb432fd0ca14698e9a2da91ae205b6f1e732
Vgbire <1053254202@qq.com>
2025-03-20 16:15:51 +0800

    feat: test

--- example/src/index.js
+++ example/src/index.js
@@ -2,8 +2,6 @@ import { createRoot } from 'react-dom/client';
 import App from './App';
 import { changeVersion } from '@vgbire/react-keep-alive';
 
-changeVersion(6);
-
 const rootElement = document.getElementById('root');
 const root = createRoot(rootElement);
 
